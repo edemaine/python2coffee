ead3d0ca8933b8a75eca373f88578444e8ff5a68
Erik Demaine <edemaine@mit.edu>
2019-01-13 10:15:01 -0500

    Add test file

---
+++ test.py
@@ -0,0 +1,17 @@
+# Python 2.7 code
+'# Hello {}, your age is {}'.format(name, age)
+### This is a comment
+for item in range(17): # up to 16
+  print item if item % 2 == 1 else item/2
+for item in range(2, 17):
+  print str(item), 'eh?'
+for item in range(2, 17, 3):
+  print hex(item + 1)
+while True:
+  item = f()
+  if item:
+    print item
+  elif forever:
+    continue
+  else:
+    break
